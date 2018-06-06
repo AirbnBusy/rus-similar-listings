7ead034fd00d524ea240a87daace30d16adf3ce2
Ruslan Farutdinov <ruslanfarutdinov@gmail.com>
2018-06-05 17:02:09 -0700

    Setup the client AJAX request

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const parser = require('body-parser');
 const path = require('path');
-const axios = require('axios');
 const db = require('./../database/index.js');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/../client/dist')));
+app.use('/rooms/:id', express.static(path.join(__dirname, '/../client/dist')));
 
 const PORT = 3004;
 app.listen(PORT, () => {
