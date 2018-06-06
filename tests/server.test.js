c0e3a7fe83410532773ad3466c5f4d0efa50fc07
Ruslan Farutdinov <ruslanfarutdinov@gmail.com>
2018-06-06 15:05:25 -0700

    Write server/API tests

--- tests/server.test.js
+++ tests/server.test.js
@@ -8,4 +8,35 @@ describe('Test the /rooms/:id/ path', () => {
       done();
     });
   });
+
+  test('It should respond to the GET request with static files', (done) => {
+    request(app).get('/rooms/1034').then((response) => {
+      expect(response.type).toBe('text/html');
+      done();
+    });
+  });
+});
+
+describe('Test the /api/similar-listings/:id/ path', () => {
+  test('It should respond to the GET request with status code 200', (done) => {
+    request(app).get('/api/similar-listings/1079/').then((response) => {
+      expect(response.statusCode).toBe(200);
+      done();
+    });
+  });
+
+  test('It should respond to the GET request with a JSON file', (done) => {
+    request(app).get('/api/similar-listings/1079').then((response) => {
+      expect(response.type).toBe('application/json');
+      done();
+    });
+  });
+
+  // FIX THIS TEST TO LENGTH = 12, AFTER FIXING THE DATABASE
+  test('It should respond to the GET request for a listing with 12 similar listings', (done) => {
+    request(app).get('/api/similar-listings/1079').then((response) => {
+      expect(response.body.length).toBe(11);
+      done();
+    });
+  });
 });
