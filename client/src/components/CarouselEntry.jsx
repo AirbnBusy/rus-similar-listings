d8459310b12bdb380f89ae9b71f730ed90d7d606
Ruslan Farutdinov <ruslanfarutdinov@gmail.com>
2018-06-09 10:54:09 -0700

    Add heart to top right corner

--- client/src/components/CarouselEntry.jsx
+++ client/src/components/CarouselEntry.jsx
@@ -7,15 +7,22 @@ const CarouselEntry = ({ listing }) => {
   listing.beds === 1 ? bed = 'BED' : bed = 'BEDS';
 
   return (
-    <div>
-    <img className={styles.photo} src={`https://airbnb-similar-listings-photos.s3.amazonaws.com/${listing.photo}`}/>
-    <p>{`${listing.listing_size_description}`} &bull; {`${listing.beds} ${bed}`}</p>
-    <p>{listing.listing_header}</p>
-    <p>{`$${listing.price} per night`}</p>
-    <div>
-      <ReactStars className={styles.stars} count={5} value={listing.avg_rating} edit={false} color2={'#0897A6'} size={10} />
-      <span>{listing.number_of_reviews}</span>
-    </div>
+    <div className={styles.photoContainer}>
+      <img
+        className={styles.photo}
+        src={`https://airbnb-similar-listings-photos.s3.amazonaws.com/${listing.photo}`}
+      />
+      <img
+        className={styles.heart}
+        src="https://airbnb-similar-listings-photos.s3.amazonaws.com/heart.png"
+      />
+      <p>{`${listing.listing_size_description}`} &bull; {`${listing.beds} ${bed}`}</p>
+      <p>{listing.listing_header}</p>
+      <p>{`$${listing.price} per night`}</p>
+      <div>
+        <ReactStars className={styles.stars} count={5} value={listing.avg_rating} edit={false} color2={'#0897A6'} size={10} />
+        <span>{listing.number_of_reviews}</span>
+      </div>
     </div>
   );
 }
