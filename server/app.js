29b7e0b860fae7ec12baf7ceee93779ec05d900d
Ruslan Farutdinov <ruslanfarutdinov@gmail.com>
2018-11-18 13:20:33 -0500

    Rewrite getSimilarListings using async/await

--- server/app.js
+++ server/app.js
@@ -9,26 +9,40 @@ app.use(cors());
 
 app.use('/rooms/:id', express.static(path.join(__dirname, '/../client/dist')));
 
+// This function is for the asyncGetSimilarListings function bellow
+function getsimilarListings(ids) {
+  return new Promise((resolve, reject) => {
+    const similarListingIds = [];
+    ids.forEach((similarListing) => {
+      similarListingIds.push(similarListing.similar_listing_id);
+    });
+    db.connection.query(
+      `SELECT * FROM listings WHERE id IN (${similarListingIds})`,
+      (err, data) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(data);
+      }
+    );
+  });
+}
+
+async function asyncGetSimilarListings(id) {
+  let similarListingIds;
+  let similarListings;
+  try {
+    similarListingIds = await db.getSimilarListingsIds(id);
+    similarListings = await getsimilarListings(similarListingIds);
+  } catch (err) {
+    console.log(`DB query failed, here is the error: ${err}`);
+  }
+  return similarListings;
+}
+
 app.get('/api/similar-listings/:id', (req, res) => {
   const { params } = req;
-  db.getSimilarListingsIds(params.id)
-    .then((results) => {
-      return new Promise((resolve, reject) => {
-        const similarListingIds = [];
-        results.forEach((similarListing) => {
-          similarListingIds.push(similarListing.similar_listing_id);
-        });
-        db.connection.query(
-          `SELECT * FROM listings WHERE id IN (${similarListingIds})`, 
-          (err, data) => {
-            if (err) {
-              reject(err);
-            }
-            resolve(data);
-          }
-        );
-      });
-    })
+  asyncGetSimilarListings(params.id)
     .then((data) => {
       res.status(200).send(data);
     })
